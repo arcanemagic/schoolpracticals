ec9952480836384652a873e29f35d9836c22b0e4
Dhruv Jain <dhruv.robots@gmail.com>
2018-06-04 22:59:31 +0530

    Initial programs

---
+++ q15.py
@@ -0,0 +1,16 @@
+"""
+Dhruv Jain
+XI-D
+Question 3
+"""
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+while(a != b):
+    if(a > b):
+        a = a-b
+    else:
+        b = b-a
+
+print("HCF of the two numbers is:", a)
