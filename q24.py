ccd95883f8527751f6945c39ca455684d1f94f93
Dhruv Jain <39631663+arcanemagic@users.noreply.github.com>
2018-08-30 13:37:03 +0530

    Update q24.py

--- q24.py
+++ q24.py
@@ -1,30 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug 24 12:50:01 2018
-
-@author: amity
+Dhruv Jain
+XI-D
+Question 24
 """
 
-s = input("Enter string to encrypt/decrypt: ")
-c = input("Encrypt(E)/Decrypt(D)? ")
-n = int(input("Enter rotation number: "))
-x = ""
-if c.lower() == "e":
-    for i in range(len(s)):
-        if s[i].isspace():
-            x+= " "
-            continue
-        if (ord(s[i])+n) > ord("z"):
-            x += chr(((ord(s[i])+n)-ord("z"))%26 + ord("a")-1)
-        else:
-            x += chr(ord(s[i])+n)
-elif c.lower() == "d":
-    for i in range(len(s)):
-        if s[i].isspace():
-            x+= " "
-            continue
-        if (ord(s[i])-n) < ord("a"):
-            x += chr((ord("z"))+((ord("a")-ord(s[i])-n))+3)
-        else:
-            x += chr(ord(s[i])-n)
-print(x)
+s = input("Enter string: ")
+a = input("Encrypt/Decrypt?: ")
+try:
+    b = int(input("Enter rotation number: "))
+except:
+    b = 13
+c = ""
+
+if a.lower() == "d":
+    b = -1*b
+for i in range(len(s)):
+    if s[i].isspace() == False:
+        d = ord(s[i])+b
+        if d>ord("z"):
+            d=(d-ord('Z'))%26 +ord('A')-1
+        elif d<ord('A'):
+            d=-1*((-d+ord('A'))%26) +ord('Z')+1
+        c+=chr(d)
+    else:
+        c+=' '
+print(c)
