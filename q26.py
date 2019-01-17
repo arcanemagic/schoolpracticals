319d0f25f9104de75aa09c1a6df4202d2fd6640b
Dhruv Jain <39631663+arcanemagic@users.noreply.github.com>
2019-01-17 22:19:46 +0530

    Update q26.py

--- q26.py
+++ q26.py
@@ -4,14 +4,39 @@ XI-D
 Question 26
 """
 
-n = int(input('Enter '))
-l = [n]
-
+import copy
+n = int(input("Enter number to start: "))
+l=[n]
 while n != 1:
     if n%2 == 0:
-        n = int(n/2)
+        n=n//2
+        l.append(n)
     else:
-        n = 3*n+1
-    l.append(n)
-
+        n = (n*3)+1
+        l.append(n)
 print(l)
+
+print("Length:", len(l))
+
+lbub = copy.deepcopy(l)
+bub = 0
+for i in range(0,len(lbub)):
+    for j in range(0,len(lbub)-i-1):
+        if lbub[j]>lbub[j+1]:
+            lbub[j],lbub[j+1]=lbub[j+1],lbub[j]
+            bub+=1
+print("Bubble Sorted: ", lbub)
+print("Bubble Sorting Efficiency: ", bub)
+
+lins = copy.deepcopy(l)
+ins = 0
+for i in range(1,len(lins)):
+    a=lins[i]
+    j=i-1
+    while (a<lins[j]) and j>-1:
+        lins[j+1]=lins[j]
+        j=j-1
+        ins+=1
+    lins[j+1]=a
+print("Insertion Sorted: ", lins)
+print("Insertion Sorting Efficiency: ", ins) 
