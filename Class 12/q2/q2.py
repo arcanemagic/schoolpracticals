80f874b29e311e1bb13a117d5a39221b740ab383
Dhruv Jain <dhruv.robots@gmail.com>
2019-07-02 23:52:47 +0530

    Initial upload

---
+++ Class 12/q2/q2.py
@@ -0,0 +1,30 @@
+"""
+Dhruv Jain
+XII-D
+Question 2
+"""
+
+n = input("Enter number: ")
+
+def count(n):
+    return len(n)
+
+def reverse(n):
+    return n[::-1]
+
+def hasdigit(n):
+    a = input("Enter digit to check: ")
+    return a in n
+
+def show(n):
+    n = int(n)
+    result = []
+    divider = 10
+    while divider < n:
+        temp = n % divider
+        if temp != 0:
+            result.insert(0, str(temp))
+        n -= temp
+        divider *= 10
+    result.insert(0, str(n))
+    return '+'.join(result)
