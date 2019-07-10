9219ed2a7ffb36274bca2a453a4d6cdad6e54b01
Dhruv Jain <39631663+arcanemagic@users.noreply.github.com>
2019-07-10 11:11:24 +0530

    Add files via upload

--- Class 12/q7/q7.py
+++ Class 12/q7/q7.py
@@ -3,3 +3,19 @@ Dhruv Jain
 XII-D
 Question 7
 """
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+x = np.arange(0,11,0.1)
+y1 = np.exp(-1*x/10)*np.sin(math.pi*x)
+y2 = x*np.exp(-1*x/3)
+
+plt.plot(x, y1, label="f(x)")
+plt.plot(x, y2, label="g(x)")
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('y')
+plt.savefig("Graph.jpg")
+plt.show()
