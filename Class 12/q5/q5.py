7986d5d71fb9fb45d7858a38086b83468b85bf86
Dhruv Jain <dhruv.robots@gmail.com>
2019-07-12 00:16:00 +0530

    Update

--- Class 12/q5/q5.py
+++ Class 12/q5/q5.py
@@ -7,37 +7,15 @@ Question 5
 n = int(input("Enter number in decimals: "))
 c = input("Choose B for binary, O for octal, H for hexadecimal: ").upper()
 
-def binary(n):
-    if n < 2:
-        return str(n)
-    else:
-        return str(n%2) + binary(n//2)
+def toStr(n, base):
+	convertString = "0123456789ABCDEF"
+	if n < base:
+		return convertString[n]
+	else:
+		return toStr(n//base, base) + convertString[n%base]
 
-def octal(n):
-    if n < 8:
-        return str(n)
-    else:
-        return str(n%8) + octal(n//8)
+if c == "B": c= 2
+if c == "O": c = 8
+if c == "H": c = 16
 
-def hex(n):
-    if n < 16:
-        if n > 9:
-            n = chr(ord("A")+n-10)
-        return str(n)
-    else:
-        a = n%16
-        if a>9:
-            a = chr(ord("A")+a-10)
-        return str(a) + hex(n//16)
-
-if c == "B":
-    print(binary(n)[::-1])
-
-elif c == "O":
-    print(octal(n)[::-1])
-
-elif c == "H":
-    print(hex(n)[::-1])
-
-else:
-    print("Number system not recognized!")
+print(toStr(n,c))
