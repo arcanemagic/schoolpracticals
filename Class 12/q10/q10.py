21b90cf242254c278ac6995c14348165282e685b
Dhruv Jain <39631663+arcanemagic@users.noreply.github.com>
2019-07-26 10:42:46 +0530

    Add files via upload

--- Class 12/q10/q10.py
+++ Class 12/q10/q10.py
@@ -9,9 +9,9 @@ from tkinter import messagebox
 
 root = Tk()
 root.title("Simple Interest Calculator")
-P = IntVar()
-R = IntVar()
-T = IntVar()
+P = StringVar()
+R = StringVar()
+T = StringVar()
 Label(root, text="Enter Principal Amount: ").grid(column=0, row=0)
 Entry(root, width=20, textvariable=P).grid(column=2, row=0)
 Label(root, text="Enter Rate of Investment: ").grid(column=0, row=1)
@@ -20,9 +20,9 @@ Label(root, text="Enter Time Period: ").grid(column=0, row=2)
 Entry(root, width=20, textvariable=T).grid(column=2, row=2)
 
 def clicked():
-    p = P.get()
-    r = R.get()
-    t = T.get()
+    p = int(P.get())
+    r = int(R.get())
+    t = int(T.get())
     si = (p*r*t)/100
     messagebox.showinfo("Simple Interest", "Your interest is: "+str(si))
 Button(root, text="Calculate", command=clicked).grid(column=0, row=4)
