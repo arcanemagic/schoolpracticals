81afa2982ddde8201208fe294177c87e8c0f648b
Dhruv Jain <39631663+arcanemagic@users.noreply.github.com>
2019-09-27 10:29:59 +0530

    Add files via upload

---
+++ Class 12/DBq3/DBq3.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 10:28:22 2019
+
+@author: SCL1
+"""
+
+import mysql.connector
+
+connection = mysql.connector.connect(host='localhost',
+                                   database='arcanemagic',
+                                   user='root',
+                                   password='amity')
+cursor = connection.cursor()
+
+a = input("Insert/Display/Search records: ").lower()
+
+def insertIntoTable():
+    itemCode = input("Enter Item Code: ")
+    itemName = input("Enter Item Name: ")
+    price = float(input("Enter Price: "))
+    query = "INSERT INTO item values('%s', '%s', '%f');" % (itemCode, itemName, price)
+    cursor.execute(query)
+    connection.commit()
+    
+def showRecords():
+    query = "SELECT * FROM item;"
+    cursor.execute(query)
+    for i in cursor.fetchall():
+        print(i)
+
+def searchRecords():
+    itemCode = input("Enter Item Code: ")
+    query = "SELECT * FROM item WHERE Itemcode = '%s';" % (itemCode)
+    cursor.execute(query)
+    for i in cursor.fetchall():
+        print(i)
+
+if a == "insert":
+    insertIntoTable()
+elif a == "display":
+    showRecords()
+elif a == "search":
+    searchRecords()
+else:
+    print("Unknown operation")
